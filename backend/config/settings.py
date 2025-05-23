6cac41dc605433bd1b2c1445f6c144f9e121661d
ebrahimElzook <113642589+ebrahimElzook@users.noreply.github.com>
2025-05-24 00:09:22 +0300

    remove

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -114,9 +114,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dangerebrahim@gmail.com'
-EMAIL_HOST_PASSWORD = 'wpam ojgc cmbw vfgd'
-DEFAULT_FROM_EMAIL = 'dangerebrahim@gmail.com'
+
 
 # CORS Settings
 CORS_ALLOW_CREDENTIALS = True
